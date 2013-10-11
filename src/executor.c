Rela-X/rlang

commit d51fc41b90784f71454c2478d153dab9b53ae690

adds Sets to RLang

src/executor.c
@@ -14,7 +14,6 @@ static void block(const Ast *);
 static void ifstat(const Ast *);
 static void whilestat(const Ast *);
 static void declaration(const Ast *);
-static void function(const Ast *);
 static void returnstat(const Ast *);
 
 static Value * eval(const Ast *);
@@ -59,7 +58,7 @@ ast_execute(const Ast *ast) {
 static
 void
 exec(const Ast *ast) {
-printf("executing "); pn(ast);
+//printf("executing "); pn(ast);
 	Value *v;
 	switch(ast->class) {
 	case N_BLOCK:
@@ -73,9 +72,6 @@ printf("executing "); pn(ast);
 		return;
 	case N_DECLARATION:
 		declaration(ast);
-		return;
-	case N_FUNCTION:
-		function(ast);
 		return;
 	case N_RETURN:
 		returnstat(ast);
@@ -107,13 +103,8 @@ printf("executing "); pn(ast);
 	case N_SET:
 	case N_R:
 		v = eval(ast);
-print_tree(stdout, ast); printf(" evaluates to "); if(v != NULL) { pv(v); value_free(v); }
-		if(v != NULL) {
-//			value_free(v);
-		} else {
-			printf("v=null: "); pn(ast);
-			assert(false);
-		}
+do { print_tree(stdout, ast); printf(" evaluates to "); pv(v); } while(0);
+		value_free(v);
 		return;
 	}
 printf("EXECFAIL %d ", ast->class); pn(ast);
@@ -173,12 +164,6 @@ declaration(const Ast *declaration) {
 
 	Memory *m = mem_new(id->symbol);
 	memspace_store(current_memspace, m);
-}
-
-static
-void
-function(const Ast *function) {
-	// TODO
 }
 
 static
@@ -706,7 +691,28 @@ static
 Value *
 _set(const Ast *expr) {
 	Value *rval = value_new();
-	value_set_set(rval, rf_set_from_string(expr->value));
+
+	int n = 0;
+	for(Ast *c = expr->child; c != NULL; c = c->next)
+		n++;
+
+	int eidx = -1;
+	rf_SetElement *elems[n];
+	for(Ast *c = expr->child; c != NULL; c = c->next) {
+		Value *v;
+		switch(c->class) {
+		case N_STRING:
+			elems[++eidx] = rf_set_element_new_string(c->value);
+			break;
+		case N_SET:
+			v = _set(c);
+			elems[++eidx] = rf_set_element_new_set(v->as_Set);
+			value_free(v);
+			break;
+		}
+	}
+
+	value_set_set(rval, rf_set_new(n, elems));
 
 	return rval;
 }

src/builtins.c
@@ -118,6 +118,20 @@ builtin_r2str(Scope *args, MemorySpace *memspace) {
 
 static
 Value *
+builtin_set_is_subset(Scope *args, MemorySpace *memspace) {
+	Value *arg[] = {
+		get_value_by_name(args, memspace, "0"),
+		get_value_by_name(args, memspace, "1"),
+	};
+	Value *rval = value_new();
+
+	value_set_bool(rval, rf_set_is_subset(arg[0]->as_Set, arg[1]->as_Set));
+
+	return rval;
+}
+
+static
+Value *
 builtin_relation_is_homogeneous(Scope *args, MemorySpace *memspace) {
 	Value *v = get_value_by_name(args, memspace, "0");
 	Value *rval = value_new();
@@ -187,6 +201,8 @@ init_builtin_functions(Scope *builtin_scope) {
 	def_function(T_STRING, "float2str", &builtin_float2str, T_FLOAT);
 	def_function(T_STRING, "set2str", &builtin_set2str, T_SET);
 	def_function(T_STRING, "r2str", &builtin_r2str, T_R);
+
+	def_function(T_BOOL, "set_is_subset", &builtin_set_is_subset, T_SET, T_SET);
 
 	def_function(T_BOOL, "relation_is_homogeneous", &builtin_relation_is_homogeneous, T_R);
 	def_function(T_BOOL, "relation_is_reflexive", &builtin_relation_is_reflexive, T_R);

src/print.c
@@ -56,6 +56,15 @@ void print_node(FILE *f, const Ast *ast) {
 	case N_STRING:
 		fprintf(f, "%s", ast->value);
 		break;
+	case N_SET:
+		fprintf(f, "{");
+		for(Ast *c = ast->child; c != NULL; c = c->next) {
+			fprintf(f, " ");
+			print_node(f, c);
+		}
+		fprintf(f, " ");
+		fprintf(f, "}");
+		break;
 	default:
 		if(ast->class >= len(node_name_table)) {
 			fprintf(f, "%s:%d:%s TODO %d", __FILE__, __LINE__, __func__, ast->class);
@@ -101,11 +110,13 @@ print_tree(FILE *f, const Ast *ast) {
 	assert(ast != NULL);
 
 	switch(ast->class) {
+	case N_IDENTIFIER:
 	case N_BOOLEAN:
 	case N_INTEGER:
 	case N_FLOAT:
 	case N_STRING:
-	case N_IDENTIFIER:
+	case N_SET:
+	case N_R:
 		print_node(f, ast);
 		return; // no children
 	default:
@@ -185,6 +196,16 @@ print_value(FILE *f, const Value *value) {
 	case T_STRING:
 		fprintf(f, "%s", value->as_String);
 		break;
+	case T_SET: {
+		char *str = rf_set_to_string(value->as_Set);
+		fprintf(f, "%s", str);
+		free(str);
+		} break;
+	case T_R: {
+		char *str = rf_relation_to_string(value->as_Relation);
+		fprintf(f, "%s", str);
+		free(str);
+		} break;
 	default:
 		fprintf(f, "%s:%d:%s TODO ", __FILE__, __LINE__, __func__);
 		print_type(f, value->type);

src/rlang.l
@@ -1,5 +1,6 @@
-%option noyywrap
 %option bison-bridge
+%option noyywrap
+%option stack
 
 %{
 #include <stdbool.h>
@@ -39,6 +40,7 @@ nondigit	[_[:alpha:]]
 c99_id		([_[:alpha:]]|{ucn})([_[:alnum:]]|{ucn})*
 
 %x STR
+%x SET
 
 %x COMMENT
 %x BLOCKCOMMENT
@@ -91,17 +93,25 @@ c99_id		([_[:alpha:]]|{ucn})([_[:alnum:]]|{ucn})*
 {c99_floating_point}	{ VALUE(FLOAT); }
 {c99_id}		{ VALUE(IDENTIFIER); }
 
-["]		{ BEGIN(STR); }
-<STR>[^"]*	{ VALUE(STRING); }
-<STR>["]	{ BEGIN(INITIAL); }
+<INITIAL>["]		{ yy_push_state(STR); }
+<STR>{
+	[^"]*		{ VALUE(STRING); }
+	["]		{ yy_pop_state(); }
+}
 
-.		{ printf("UNHANDLED TOKEN \"%s\"\n", yytext); }
+<INITIAL>"//"		{ yy_push_state(COMMENT); }
+<COMMENT>{
+	.		{ }
+	\n		{ yy_pop_state(); }
+}
 
-"//"		{ BEGIN(COMMENT); }
-<COMMENT>\n	{ BEGIN(INITIAL); }
-<COMMENT>.	{ }
-"/*"			{ BEGIN(BLOCKCOMMENT); }
-<BLOCKCOMMENT>"*/"	{ BEGIN(INITIAL); }
-<BLOCKCOMMENT>.		{ }
+<*>"/*"			{ yy_push_state(BLOCKCOMMENT); }
+<BLOCKCOMMENT>{
+	.		{ }
+	"*/"		{ yy_pop_state(); }
+}
+
+.	{ printf("UNHANDLED TOKEN \"%s\"\n", yytext); }
+
 %%
 

src/rlang.y
@@ -39,9 +39,11 @@ yyerror(YYLTYPE *llocp, Ast **ast, const char *yymsg) {
 %type  <node>  vardecl function function_args
 %type  <node>  expr assign_expr call_expr call_args
 %type  <node>  boolean_op arithmetic_comp arithmetic_op
-%type  <node>  identifier
+%type  <node>  identifier value
+%type  <node>  set set_elements set_element
 
-%token <value> BOOLEAN INTEGER FLOAT STRING IDENTIFIER
+%token <value> IDENTIFIER
+%token <value> BOOLEAN INTEGER FLOAT STRING
 
 %token ASSIGN
 %token IF ELSE
@@ -123,10 +125,8 @@ expr            : LPAREN expr RPAREN            { $$ = $2; }
                 | expr boolean_op expr          { $$ = $2; ast_append_child_all($$, $1, $3); }
                 | expr arithmetic_comp expr     { $$ = $2; ast_append_child_all($$, $1, $3); }
                 | expr arithmetic_op expr       { $$ = $2; ast_append_child_all($$, $1, $3); }
-                | BOOLEAN                       { $$ = ast_new(N_BOOLEAN); $$->value = $1; }
-                | INTEGER                       { $$ = ast_new(N_INTEGER); $$->value = $1; }
-                | FLOAT                         { $$ = ast_new(N_FLOAT); $$->value = $1; }
-                | STRING                        { $$ = ast_new(N_STRING); $$->value = $1; }
+                | value
+                | set
 /*
                 | INC identifier
                 | DEC identifier
@@ -168,27 +168,22 @@ arithmetic_op   : ADD   { $$ = ast_new(N_ADD); }
                 | MOD   { $$ = ast_new(N_MOD); }
                 ;
 
-/* -- */
+value           : BOOLEAN                       { $$ = ast_new(N_BOOLEAN); $$->value = $1; }
+                | INTEGER                       { $$ = ast_new(N_INTEGER); $$->value = $1; }
+                | FLOAT                         { $$ = ast_new(N_FLOAT); $$->value = $1; }
+                | STRING                        { $$ = ast_new(N_STRING); $$->value = $1; }
+                ;
 
-//relation        : domain 'x' domain '=' table            // { printf("Relation(%s, %s, %s)\n", $1, $3, $5); }
-//                ;
-//domain          : '{' domainElements '}'                 // { printf("Domain(%s)\n", $2); }
-//                ;
-//domainElements  : /* empty */
-//                | domainElement domainElements
-//                ;
-//domainElement   : domain /* domains can be nested */
-//                | identifier   /* [a-z] */
-//                ;
-//table           : '{' tableRows '}'                      // { printf("Table(%s)\n", $2); }
-//                ;
-//tableRows       : tableRow
-//                | tableRow '|' '|' tableRows
-//                ;
-//tableRow        : binBool /* [01 ]* (can be empty)*/
-//                ;
-//
-//binBool         :;
+set             : LBRACE set_elements RBRACE    { $$ = $2; }
+                ;
 
+set_elements    : /* empty */                   { $$ = ast_new(N_SET); }
+                | set_element                   { $$ = ast_new(N_SET); ast_append_child($$, $1); }
+                | set_elements set_element      { ast_append_child($1, $2); }
+                ;
+
+set_element     : value                         { $1->class = N_STRING; }
+                | identifier                    { $1->class = N_STRING; }
+                | set
+                ;
 %%
-

commit 8d59c48acc468dddffa92dbb44ce2d614f1fafbc

fixes error introduced in d51fc41b90784f71454c2478d153dab9b53ae690

src/executor.c
@@ -73,6 +73,8 @@ exec(const Ast *ast) {
 	case N_DECLARATION:
 		declaration(ast);
 		return;
+	case N_FUNCTION:
+		return;
 	case N_RETURN:
 		returnstat(ast);
 		return;