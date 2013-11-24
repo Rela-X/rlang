Rela-X/rlang

commit d074919f6e7afe19e62d0c91e2e7599bcfda7a36

fixed leak in parser

src/rlang.y
@@ -170,7 +170,7 @@ value           : BOOLEAN                       { $$ = ast_new(N_BOOLEAN); $$->v
                 | STRING                        { $$ = ast_new(N_STRING); $$->value = $1; }
                 ;
 
-relation        : set IDENTIFIER set COLON rtable       { $$ = ast_new(N_R); ast_append_child_all($$, $1, $3, $5); }
+relation        : set IDENTIFIER set COLON rtable       { $$ = ast_new(N_R); ast_append_child_all($$, $1, $3, $5); ast_free($2); }
                 ;
 
 set             : LBRACE set_elements RBRACE    { $$ = $2; }

src/rlang.l
@@ -119,7 +119,7 @@ c99_id		([_[:alpha:]]|{ucn})([_[:alnum:]]|{ucn})*
 	"0"		{ VALUE(BOOLEAN); }
 	"1"		{ VALUE(BOOLEAN); }
 	"|"		{ TOKEN(VBAR); }
-	[[:space:]]	{ }
+	[[:space:]]*	{ }
 	"]"		{ yy_pop_state(); TOKEN(RBRACKET); }
 }
 

commit dc43795921a2d8287e25f5e82f6922cdbe034ae2

fixes a bug introduced in d074919f6e7afe19e62d0c91e2e7599bcfda7a36

src/rlang.y
@@ -170,7 +170,7 @@ value           : BOOLEAN                       { $$ = ast_new(N_BOOLEAN); $$->v
                 | STRING                        { $$ = ast_new(N_STRING); $$->value = $1; }
                 ;
 
-relation        : set IDENTIFIER set COLON rtable       { $$ = ast_new(N_R); ast_append_child_all($$, $1, $3, $5); ast_free($2); }
+relation        : set IDENTIFIER set COLON rtable       { if($2[0] != 'x' || $2[1] != '\0') YYABORT; free($2); $$ = ast_new(N_R); ast_append_child_all($$, $1, $3, $5); }
                 ;
 
 set             : LBRACE set_elements RBRACE    { $$ = $2; }